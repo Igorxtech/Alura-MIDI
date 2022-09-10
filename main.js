993e4839bdfd5bae46cef6a17718da935aa6dbe3
Igor Bezerra Leandro <igorigrleandro@gmail.com>
2022-09-10 08:45:56 -0300

    Substituindo estrutura while por for

--- main.js
+++ main.js
@@ -4,11 +4,7 @@ function tocaSom(idElementoAudio) {
 
 const listaDeTeclas = document.querySelectorAll('.tecla');
 
-
-
-let contador = 0;
-
-while(contador < listaDeTeclas.length) {
+for (let contador = 0; contador < listaDeTeclas.length; contador++;) {
 
     const tecla = listaDeTeclas[contador];
     const instrumento = tecla.classList[1];
@@ -18,8 +14,5 @@ while(contador < listaDeTeclas.length) {
     tecla.onclick = function () {
         tocaSom(idAudio);
     };
-
-    contador++;
-    console.log(contador);
 
 }
