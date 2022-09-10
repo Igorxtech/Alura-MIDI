6502320ac1b791f5e0ac71f0f765acd1ffd1bb6d
Igor Bezerra Leandro <igorigrleandro@gmail.com>
2022-09-10 08:48:01 -0300

    Corrigindo erro de sintaxe do laço for

--- main.js
+++ main.js
@@ -4,7 +4,7 @@ function tocaSom(idElementoAudio) {
 
 const listaDeTeclas = document.querySelectorAll('.tecla');
 
-for (let contador = 0; contador < listaDeTeclas.length; contador++;) {
+for (let contador = 0; contador < listaDeTeclas.length; contador++) {
 
     const tecla = listaDeTeclas[contador];
     const instrumento = tecla.classList[1];
