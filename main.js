f1f35b08656d1c1a7300f2245cad230e23add3cc
Igor Bezerra Leandro <igorigrleandro@gmail.com>
2022-09-15 15:27:13 -0300

    Projeto finalizado

--- main.js
+++ main.js
@@ -1,5 +1,15 @@
-function tocaSom(idElementoAudio) {
-    document.querySelector(idElementoAudio).play();
+function tocaSom(seletorAudio) {
+    const elemento = document.querySelector(seletorAudio);
+
+    
+    if (elemento && elemento.localName === 'audio') {
+        elemento.play();
+
+    }
+    else {
+        alert('Elemento não encontrado ou seletor inválido');
+
+    }
 }
 
 const listaDeTeclas = document.querySelectorAll('.tecla');
@@ -14,5 +24,15 @@ for (let contador = 0; contador < listaDeTeclas.length; contador++) {
     tecla.onclick = function () {
         tocaSom(idAudio);
     };
+
+    tecla.onkeydown = function (evento) {
+        if (evento.code === "Enter" || evento.code === "Space"){
+            tecla.classList.add('ativa');
+        }
+    }
+
+    tecla.onkeyup = function () {
+        tecla.classList.remove('ativa');
+    }
 
 }
